8d236a635f3857b50ff86fde1e2408ba5bf9acc4
Crowdbotics <team@crowdbotics.com>
2019-06-27 02:39:55 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.salad_club_5262.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/salad_club_5262/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for salad_club_5262 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salad_club_5262.settings")
+
+application = get_wsgi_application()
